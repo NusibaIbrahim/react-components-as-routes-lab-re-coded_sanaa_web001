ad27cb08fd85bdcd9282ea076425404408c0bebf
Nusiba Ibrahim <ainusiba@gmail.com>
2020-09-17 07:33:12 +0000

    Automatically backed up by Learn

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,21 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor => {
+        return <div key={actor.name}>
+          <h3>{actor.name}</h3>
+          <ul><strong>Movies</strong></ul>
+          <ul>
+            {actor.movies.map(movie => {
+              return <li key={movie}>
+                {movie}
+              </li>
+            })}
+          </ul>
+          <hr></hr>
+        </div>
+      })}
     </div>
   );
 };
