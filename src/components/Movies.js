19578b1ebfbad029745cc33d21dbb9660edb61a6
Nusiba Ibrahim <ainusiba@gmail.com>
2020-09-17 07:31:31 +0000

    Automatically backed up by Learn

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,7 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-     <h1>Movies Page</h1>
+      <h1>Movies Page</h1>
       {movies.map(movie => {
         return <div key={movie.title}>
           <h3>{movie.title}</h3>
